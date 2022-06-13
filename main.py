583310208d4a7cc86ccce4d535989de45dec410d
Steven Ford <evensteven01@gmail.com>
2022-06-13 16:50:53 -0400

    Added function to ask get person details from command line

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 def runPersonDatabase():
-	pass
+	
+	for i in range(3):
+		userInput = inputUser()
+		personDict = createPerson(*userInput)
+		print(personDict)
+
+def inputUser():
+	firstName = input("What is the users first name: ")
+	lastName = input("What is the users last name: ")
+	occupation = input("What is the users occupation: ")
+	return firstName, lastName, occupation
+
+def createPerson(firstName, lastName, occupation):
+	return {
+		"FirstName": firstName,
+		"LastName": lastName,
+		"Occupation": occupation,
+	}
 
 if __name__ == "__main__":
 	runPersonDatabase()
