18e0d6c456f864a1021f353db3b9c37c965e3484
Ryan Setaruddin <coolry1998@gmail.com>
2022-06-21 18:20:04 -0400

    I created person database

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from models.person import Person
+from models.personDatabase import PersonDatabase
+
+personDB = PersonDatabase()
 
 def runPersonDatabase():
-	for i in range(3):
+	for i in range(1):
 		userInput = inputUser()
-		personDict = createPerson(*userInput)
-		print(personDict)
+		person = createPerson(*userInput)
+		print(person)
+		personDB.createPerson(person)
 
 def inputUser():
 	firstName = input("What is the users first name: ")
 	lastName = input("What is the users last name: ")
-	occupation = input("What is the users occupation: ")
 	age = input("What is the users age: ")
 	gender = input("What is the users gender: ")
+	occupation = input("What is the users occupation: ")
 	return firstName, lastName, occupation, age, gender
 
-def createPerson(firstName: str, lastName: str, occupation: str, age: int, gender: str) -> Person:
+def createPerson(firstName: str, lastName: str, age: int, gender: str, occupation: str) -> Person:
 	return Person(firstName, lastName, occupation, age, gender)
 
 if __name__ == "__main__":
